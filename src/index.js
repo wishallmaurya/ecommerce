3ed7ba96c12bd8b205d813bbfe94eca8720a2cec
Vishal Kumar Maurya <vm7740014@gmail.com>
2023-04-02 01:31:29 +0530

    deploy2

--- src/index.js
+++ src/index.js
@@ -8,8 +8,11 @@ import categoryRoute from './routes/categoryRoute.js'
 import productRoute from  './routes/productRoutes.js'
 import cors from 'cors'
 import path from 'path'
+import {fileURLToPath} from 'url'
 dotenv.config();
 
+const __filename=fileURLToPath(import.meta.url)
+const __dirname=path.dirname(__filename)
 const app=express();
 app.use(cors())
 app.use(express.json())
